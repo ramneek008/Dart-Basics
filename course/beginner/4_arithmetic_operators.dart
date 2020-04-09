169fe8843464c50bf13a72a8ca49a97e2043b28c
Ramneek Singh <48586823+ramneek008@users.noreply.github.com>
2020-04-09 19:07:17 +0530

    arithmetic operators

--- course/beginner/4_arithmetic_operators.dart
+++ course/beginner/4_arithmetic_operators.dart
@@ -1,7 +1,26 @@
+import 'dart:math';
+
 main(){
 
   //Operators
 
-  
+  var num1 = 10;
+  var num2 = 5;
+
+  var add = num1 + num2;
+  var sub = num1 - num2;
+  var mul = num1 * num2;
+  var div = num1 / num2;
+  var mod = num1 % num2;
+
+  print("num1 + num2 = ${add}");
+  print("num1 - num2 = ${sub}");
+  print("num1 * num2 = ${mul}");
+  print("num1 / num2 = ${div}");
+  print("num1 % num2 = ${mod}");
+
+  var edge = 5;
+  var sa = 6 * pow(edge,2);
+  print("Surface Area : ${sa}");
 
 }
