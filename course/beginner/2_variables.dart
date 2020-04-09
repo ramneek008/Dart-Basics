fd5b85fcc19a6c94cc0f8145d796f7edab8d2403
Ramneek Singh <48586823+ramneek008@users.noreply.github.com>
2020-04-09 17:15:42 +0530

    variables

---
+++ course/beginner/2_variables.dart
@@ -0,0 +1,29 @@
+main(){
+
+  //Number (Integers)
+  var age = 19;
+
+  //Number (Double)
+  var weight = 64.3;
+
+  //String
+  var name = "Ramneek";
+
+  //Boolean
+  var is_male = true;
+
+  //update age
+  age = 21;
+
+  //Print
+  print(age);
+  print(name);
+
+  print("${name} is ${age} years old.");
+
+
+
+
+
+
+}
