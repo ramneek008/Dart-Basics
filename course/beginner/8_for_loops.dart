cca60ac68a90d70e770fd732f96c890619092410
Ramneek Singh <48586823+ramneek008@users.noreply.github.com>
2020-04-10 20:18:22 +0530

    for loops

---
+++ course/beginner/8_for_loops.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+main(){
+
+
+  //for loop
+
+  for(var i = 0; i <= 5; i++)
+    {
+      print(i);
+    }
+
+  for(var i=0;i<=20;i++)
+    {
+      print("Welcome user: ${i}");
+    }
+
+}
