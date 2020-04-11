89cfa4c9ae8514dea19c1a1c625408a1d2b25fbc
Ramneek Singh <48586823+ramneek008@users.noreply.github.com>
2020-04-12 01:00:44 +0530

    list

--- course/beginner/lists.dart
+++ course/beginner/lists.dart
@@ -9,4 +9,9 @@ main(){
   var list2 = [0,...list];    //using spread operator
   print("List2: $list2");
   print("Length of list2: ${list2.length}");
+
+  var superheroes = ['hulk', 'superman', 'batman', 'flash'];
+  superheroes.forEach((hero){
+    print('At position: ${superheroes.indexOf(hero)} Next hero is ${hero}');
+  });
 }
